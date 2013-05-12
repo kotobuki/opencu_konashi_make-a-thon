3db52bbf0fdd6991ea77d5e14020d4195255fd92
Shigeru Kobayashi <kotobuki@gmail.com>
2013-05-12 10:30:03 +0900

    Added the first example

---
+++ BareMinimum/BareMinimum/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  BareMinimum
+//
+//  Created by Shigeru Kobayashi on 2013/05/12.
+//  Copyright (c) 2013年 Shigeru Kobayashi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+// Import the konashi library
+#import "Konashi.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+
+    // Initialize
+    [Konashi initialize];
+    
+    // Add an observer for KONASHI_EVENT_READY events
+    [Konashi addObserver:self selector:@selector(ready) name:KONASHI_EVENT_READY];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)ready
+{
+    // Do any setups after connecting to a konashi module
+}
+
+@end
